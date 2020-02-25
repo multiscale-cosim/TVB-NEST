3b106cf09c04c1b0b22777b0dae1e97b899b42ad
Kusch Lionel <lionel.kusch@univ-amu.fr>
2020-02-25 15:01:01 +0100

    Better parameters for the co-simulation

--- test_nest/run_co-sim.py
+++ test_nest/run_co-sim.py
@@ -1,14 +1,15 @@
 from nest_elephant_tvb.simulation.run_exploration import run_exploration_2D
+import mpi4py.MPI as MPI
 import numpy as np
 from nest_elephant_tvb.parameter import test_nest as parameter_test
 
 def run_exploration(path,begin,end):
     parameter_test.param_co_simulation['co-simulation']=True
-    parameter_test.param_co_simulation['nb_MPI_nest']=8
+    parameter_test.param_co_simulation['nb_MPI_nest']=MPI.COMM_WORLD.Get_size()
     parameter_test.param_nest['total_num_virtual_procs']=parameter_test.param_co_simulation['nb_MPI_nest']
     parameter_test.param_co_simulation['nb_MPI_tvb']=1
     parameter_test.param_co_simulation['id_region_nest']=[1,2]
-    parameter_test.param_co_simulation['synchronization']=3.4
+    parameter_test.param_co_simulation['synchronization']=11.0
     run_exploration_2D(path, parameter_test, {'g':np.arange(1.0, 1.2, 0.5), 'mean_I_ext': [0.0]}, begin, end)
 
 if __name__ == "__main__":
