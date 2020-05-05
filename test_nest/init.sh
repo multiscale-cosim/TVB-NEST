20b96320eecebf42c4c5c2c74c0cb633c551aefe
Kusch Lionel <lionel.kusch@univ-amu.fr>
2020-05-05 16:27:53 +0200

    Fix init for test

--- test_nest/init.sh
+++ test_nest/init.sh
@@ -14,18 +14,18 @@ then
         module load SciPy-Stack/2019a-Python-3.6.8 mpi4py/3.0.1-Python-3.6.8
     fi
 
-    PACKAGE=../../co-simulation-tvb-nest/
-    PYTHONLIB=../lib/site_packages
-    REPERTORY_LIB_NEST=../lib/nest_run/lib64/python3.6/site-packages/
+    PACKAGE=${PWD}/../   # folder of co-simulation-tvb-nest
+    PYTHONLIB=../lib/site_packages # folder with python library
+    REPERTORY_LIB_NEST=../lib/nest_run/lib64/python3.6/site-packages/ # folder with py-nest
     export PYTHONPATH=$PYTHONPATH:$PACKAGE:$PYTHONLIB:$REPERTORY_LIB_NEST
     RUN="srun"
 else
     echo " WARNING the python path are not defene"
 
-    #PACKAGE=../../co-simulation-tvb-nest/
-    #PYTHONLIB=../lib/site_packages
-    #REPERTORY_LIB_NEST=../lib/nest_run/lib64/python3.6/site-packages/
-    #export PYTHONPATH=$PYTHONPATH:$PACKAGE:$PYTHONLIB:$REPERTORY_LIB_NEST
+#    PACKAGE=${PWD}/../   # folder of co-simulation-tvb-nest
+#    PYTHONLIB=${PWD}/../venv/lib/python3.6/site-packages # folder with python library
+#    REPERTORY_LIB_NEST=${PWD}/../lib/nest_run/lib/python3.6/site-packages/ # folder with py-nest
+    export PYTHONPATH=$PYTHONPATH:$PACKAGE:$PYTHONLIB:$REPERTORY_LIB_NEST
     RUN="mpirun"
 fi
     
