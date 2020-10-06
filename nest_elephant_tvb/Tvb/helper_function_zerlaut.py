f93fecc48020323a733d759ba892d8ffb820760a
Kusch Lionel <lionel.kusch@univ-amu.fr>
2020-10-06 08:57:00 +0200

    Function for the iEEG

---
+++ nest_elephant_tvb/Tvb/helper_function_zerlaut.py
@@ -0,0 +1,25 @@
+# https://stackoverflow.com/questions/51272288/how-to-calculate-the-vector-from-two-points-in-3d-with-python
+def multiDimenDist(point1,point2):
+   #find the difference between the two points, its really the same as below
+   deltaVals = [point2[dimension]-point1[dimension] for dimension in range(len(point1))]
+   runningSquared = 0
+   #because the pythagarom theorm works for any dimension we can just use that
+   for coOrd in deltaVals:
+       runningSquared += coOrd**2
+   return runningSquared**(1/2)
+def findVec(point1,point2,unitSphere = False):
+  #setting unitSphere to True will make the vector scaled down to a sphere with a radius one, instead of it's orginal length
+  finalVector = [0 for coOrd in point1]
+  for dimension, coOrd in enumerate(point1):
+      #finding total differnce for that co-ordinate(x,y,z...)
+      deltaCoOrd = point2[dimension]-coOrd
+      #adding total difference
+      finalVector[dimension] = deltaCoOrd
+  if unitSphere:
+      totalDist = multiDimenDist(point1,point2)
+      unitVector =[]
+      for dimen in finalVector:
+          unitVector.append( dimen/totalDist)
+      return unitVector
+  else:
+      return finalVector
