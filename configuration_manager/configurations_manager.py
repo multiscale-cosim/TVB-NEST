c76e4c3760f51090eef33acf5c9fdfbb0d3f8d74
Avnii <fahadazeemi@gmail.com>
2021-01-20 12:39:39 +0100

    Update configurations_manager.py
    Added the licensing information.

--- configuration_manager/configurations_manager.py
+++ configuration_manager/configurations_manager.py
@@ -1,3 +1,8 @@
+# Copyright 2020 Forschungszentrum Jülich GmbH and Aix-Marseille Université
+# "Licensed to the Apache Software Foundation (ASF) under one or more
+#  contributor license agreements; and to You under the Apache License,
+#  Version 2.0."
+
 from definitions import CONFIG_FILE
 from xml_parser import Parser
 from config_logger import ConfigLogger
