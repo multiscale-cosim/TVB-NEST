9c1b034b4b23cf60a4c3b656cc55da680a15386b
Avnii <fahadazeemi@gmail.com>
2021-01-20 12:42:53 +0100

    Update util.py
    Added the licensing information.

--- configuration_manager/utils/util.py
+++ configuration_manager/utils/util.py
@@ -1,3 +1,8 @@
+# Copyright 2020 Forschungszentrum Jülich GmbH and Aix-Marseille Université
+# "Licensed to the Apache Software Foundation (ASF) under one or more
+#  contributor license agreements; and to You under the Apache License,
+#  Version 2.0."
+
 import os
 import errno
 
