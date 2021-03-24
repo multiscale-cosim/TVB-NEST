80e907a47c0f60b5639cddd713f690875b0423da
Lionel Kusch <bp000266@daint102.login.cscs.ch>
2021-03-24 19:25:16 +0100

    piz-install

--- install/piz-daint/run.sh
+++ install/piz-daint/run.sh
@@ -25,19 +25,26 @@ cd $DIR || exit
 
 # load the module for all the installation
 module load daint-gpu
-module load cray-python/3.8.2.1
+module load cray-python/3.8.5.0
 
 module load CMake/3.14.5
-module load Boost/1.70.0-CrayGNU-20.11-python3
-module load GSL/2.5-CrayGNU-20.11
+module load GSL/2.5-CrayCCE-20.11
 module load python_virtualenv/15.0.3
 
-source /scratch/snx3000/bp000266/co_sim_local/TVB-NEST/lib/site_packages/bin/activate
+export DIR=/scratch/snx3000/bp000266/co_sim_local/TVB-NEST/install/piz-daint
+export INSTALL_FOLDER=${DIR}/../../lib/
+export PYTHON_FOLD=${INSTALL_FOLDER}/lib_py
+export PYTHON_LIB=${PYTHON_FOLD}/lib_py/lib64/python3.8/site-packages/
+export NEST_PY=${INSTALL_FOLDER}/nest_run/lib64/python3.8/site-packages/
+export PROJECT_LIB=${DIR}/../..
+export PATH=/scratch/snx3000/bp000266/co_sim_local/TVB-NEST/lib/nest_run/bin/:$PATH
+export LD_PRELOAD=/opt/cray/pe/libsci/20.09.1/CRAYCLANG/9.0/x86_64/lib/libsci_cray_mp.so.5:$LD_PRELOAD
 
-export PYTHONPATH=/scratch/snx3000/bp000266/co_sim_local/TVB-NEST/lib/nest_run/lib64/python3.8/site-packages/:/scratch/snx3000/bp000266/co_sim_local/TVB-NEST/lib/site_packages/lib64/python3.8/site-packages/:/scratch/snx3000/bp000266/co_sim_local/TVB-NEST/lib/site_packages/lib64/python3.8/site-packages/:/scratch/snx3000/bp000266/co_sim_local/TVB-NEST/:$PYTHONPATH
+source ${PYTHON_FOLD}/bin/activate
+
+export PYTHONPATH=${NEST_PY}:${PYTHON_LIB}:${PROJECT_LIB}:$PYTHONPATH
 
 python $DIR/../../nest_elephant_tvb/orchestrator/run.py $1
-
 desactivate
 
 # return to the calling repertory
