8af268850eba24adfc39b7bcc67d7c5d12823685
Kusch Lionel <lionel.kusch@univ-amu.fr>
2021-04-09 18:50:54 +0200

    Refactor internal thread

--- nest_elephant_tvb/translation/communication/internal.py
+++ nest_elephant_tvb/translation/communication/internal.py
@@ -1,53 +1,132 @@
+#  Copyright 2020 Forschungszentrum Jülich GmbH and Aix-Marseille Université
+# "Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements; and to You under the Apache License, Version 2.0. "
 
-class CommunicationInternAbstract():
-    def __init__(self,logger=None):
+class CommunicationInternAbstract:
+    """
+    Abstract class for internal communication
+    Limitation of spike and rate exchange
+    """
+
+    def __init__(self, logger=None):
         if logger is None:
-            raise Exception('missing logger for the internal communication')
+            raise Exception('Internal Communication : missing logger for the internal communication')
         self.logger = logger
 
     def finalise(self):
+        """
+        Procedure before finalise MPI
+        :return boolean : true if MPI finalise need to be call
+        """
         raise Exception("Not implemented")
 
-    databuffer = None
+    # Section 1 : spike trains exchange
+    databuffer = None    # shared buffer for spike exchange
+    # shape of the data inside the buffer
+    # 2 types of shape : 1) array with 1 number for continuous data
+    #                    2) arrays with multiple number for a sequence of data
     shape_buffer = None
-    send_spike_exit = False
+    send_spike_exit = False  # boolean to identify the end of the simulation
+
+    # 1.1) Send spikes trains : OPTION 1: 2 functions before and send after to use databuffer
     def send_spikes_ready(self):
+        """
+        wait until it's ready to use the buffer
+        """
         raise Exception("Not implemented")
 
     def send_spikes(self):
+        """
+        send the data inside the buffer to the receiver
+        databuffer is the buffer to send
+        shape_buffer need to be set at the good values
+        """
         raise Exception("Not implemented")
 
-    def send_spikes_trains(self,spike_trains):
+    # 1.1) Send spikes trains : OPTION 2: 1 function to write data in buffer and send it
+    def send_spikes_trains(self, spike_trains):
+        """
+        Write spike trains in buffer and send them
+        :param spike_trains: array of spikes trains
+        """
         raise Exception("Not implemented")
 
+    # 1.2) Close connection with receiver
     def send_spikes_end(self):
+        """
+        close internal connection for sending spikes
+        """
         raise Exception("Not implemented")
 
+    # 2.1) Get spikes : Option 1 : 1 function to get spikes
     def get_spikes(self):
+        """
+        wait the sender to be ready and receive the spikes trains
+        update also the shape of the data
+        :return: spike trains
+        """
         raise Exception("Not implemented")
 
+    # 2.1) Get spikes : Option 2 : 1 function to get spikes
     def get_spikes_ready(self):
+        """
+        wait until the buffer and the shape are ready to be used
+        """
         raise Exception("Not implemented")
 
+    # 2.2) End Getting spikes
+    # Mandatory to used after call the previous function
     def get_spikes_release(self):
+        """
+        end the receiving of the spikes
+        """
         raise Exception("Not implemented")
 
+    # 2.3) Close connection with the sender spikes
     def get_spikes_end(self):
+        """
+        close internal connection for getting spikes
+        """
         raise Exception("Not implemented")
 
-    get_time_rate_exit = False
-    send_time_rate_exit = False
+    # Section 2 : rate and time exchange
+    get_time_rate_exit = False   # boolean to identify the end from getting function
+    send_time_rate_exit = False  # boolean to identify the end from sending function
+
+    # 1.1) Function to get time and rate
     def get_time_rate(self):
+        """
+        wait that the data are available and return them when it's ready
+        :return: times and rates
+        """
         raise Exception("Not implemented")
 
+    # 1.2) Function to send the end receiving rate and time
+    # Mandatory to used after call the previous function
     def get_time_rate_release(self):
+        """
+        end the read of the data
+        """
         raise Exception("Not implemented")
 
+    # 1.3) Close getting time and rate
     def get_time_rate_end(self):
+        """
+        close the connection for receiving data
+        """
         raise Exception("Not implemented")
 
+    # 2.1) Send time and rate
     def send_time_rate(self, time_step, rate):
+        """
+        send time and rate
+        :param time_step: time of simulation [begin,end]
+        :param rate: rate to send
+        """
         raise Exception("Not implemented")
 
+    # 2.2) Close sending time and rate
     def send_time_rate_end(self):
+        """
+        close the connection for sending data
+        """
         raise Exception("Not implemented")
--- nest_elephant_tvb/translation/communication/internal_thread.py
+++ nest_elephant_tvb/translation/communication/internal_thread.py
@@ -1,129 +1,181 @@
+#  Copyright 2020 Forschungszentrum Jülich GmbH and Aix-Marseille Université
+# "Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements; and to You under the Apache License, Version 2.0. "
+
 import numpy as np
 import time
+from threading import Lock, BoundedSemaphore
 from nest_elephant_tvb.translation.communication.internal import CommunicationInternAbstract
-from threading import Lock,BoundedSemaphore
 
-final_barrier = BoundedSemaphore(2) # n-1 thread
-
-# can be improve with condition and semaphore or event :  https://hackernoon.com/synchronization-primitives-in-python-564f89fee732
+_final_barrier = BoundedSemaphore(2)  # n-1 thread # improvement be include in the class
 
 
 class Thread_communication(CommunicationInternAbstract):
-    def __init__(self,logger=None,buffer_write_shape=None,buffer_write_type=np.float,buffer_write_status=0,
-                 buffer_read=None,status_read=None,lock_read=None):
+    """
+    Class for using thread for the internal communication
+
+    state of the status:
+        >= 0 : shape of the buffer
+        -1 : close connection
+        -2 : end to read buffer
+    """
+    # can be improve with condition and semaphore or event :
+    # https://hackernoon.com/synchronization-primitives-in-python-564f89fee732
+
+    def __init__(self, logger=None,
+                 buffer_write_shape=None, buffer_write_type=np.float, buffer_write_status=None,
+                 buffer_read=None, status_read=None, lock_read=None):
         super().__init__(logger=logger)
-        if buffer_read is not None :
-            self.buffer_read_data = buffer_read
-            self.status_read = status_read
-            self.lock_read = lock_read
-        if buffer_write_shape is not None :
+        # set variable if reading buffer is used
+        if buffer_read is not None and status_read is not None and lock_read is not None:
             self.logger.info('end MPI init')
-            self.databuffer = np.empty(buffer_write_shape,dtype=buffer_write_type)
-            self.buffer_write_data = [self.databuffer]
-            self.status_write = [buffer_write_status]
-            self.lock_write = Lock()
-            self.shape_buffer = [0]
+            self.buffer_read_data = buffer_read  # variable contains reading buffer
+            self.status_read = status_read       # status of read buffer and it can contain the dimension of the data
+            self.lock_read = lock_read           # lock for write in the status
+        elif buffer_read is not None or status_read is not None or lock_read is not None:
+            raise Exception('missing parameter for using reading buffer')
+        # set variable if writing buffer is used
+        if buffer_write_shape is not None and buffer_write_status is not None:
+            self.logger.info('end MPI init')
+            self.databuffer = np.empty(buffer_write_shape, dtype=buffer_write_type)  # create buffer
+            self.buffer_write_data = [self.databuffer]  # variable for shared buffer
+            self.status_write = [buffer_write_status]   # status of buffer and contains the dimension of the data
+            self.lock_write = Lock()                    # lock for write in the status
+            self.shape_buffer = [0]                     # dimension of the data
+        elif buffer_write_shape is not None or buffer_write_status is not None:
+            raise Exception('missing parameter for using writing buffer')
         self.logger.info('end MPI init')
 
     def finalise(self):
+        """
+        Synchronize thread before the end of the MPI
+        :return True if it's the last one to pass
+        """
         self.logger.info('end MPI init')
-        return not final_barrier.acquire(False)
+        return not _final_barrier.acquire(False)
 
-    ## writter of the buffer
+    # Management of internal writing buffer
     def ready_write_buffer(self):
+        """
+        wait until it's ready to write in the buffer
+        :return if the communication is ending
+        """
         self.logger.info('Thread : write buffer wait '+str(self.status_write[0][0]))
-        while self.status_write[0][0] >= 0:
-            # self.logger.info('Thread : statut '+str(self.status_write[0][0]))
+        while self.status_write[0][0] >= 0:  # wait to write
             time.sleep(0.001)
-            pass
         self.logger.info('Thread : write end buffer end wait '+str(self.shape_buffer))
-        self.shape_buffer = [0]
+        self.shape_buffer = [0]  # reinitialise the buffer shape
         return self.status_write[0][0] == -1
 
-    def end_writing(self):
-        self.logger.info('Thread : end write wait '+str(self.status_write[0][0]) +" shape buffer "+str(self.shape_buffer))
-        self.buffer_write_data[0]=self.databuffer
+    def end_write_buffer(self):
+        """
+        end to write in the buffer
+        """
+        self.logger.info('Thread : end write wait '+str(self.status_write[0][0])
+                         + " shape buffer "+str(self.shape_buffer))
+        self.buffer_write_data[0] = self.databuffer  # pass the buffer in the shared variable
         with self.lock_write:
             if self.status_write[0][0] != -1:
-                self.status_write[0] = self.shape_buffer
+                self.status_write[0] = self.shape_buffer  # pass the dimension of the buffer
             else:
                 self.logger('Thread : end ')
         self.logger.info('Thread : end write end '+str(self.status_write[0][0]))
 
     def release_write_buffer(self):
+        """
+        release writing buffer and send the end of the communication
+        """
         self.logger.info('Thread : release write buffer '+str(self.status_write[0][0]))
-        while self.status_write[0][0] >= 0:
-            # self.logger.info('Thread : statut '+str(self.status_write[0][0]))
+        while self.status_write[0][0] >= 0:  # wait before change the buffer
             time.sleep(0.001)
-            pass
         with self.lock_write:
-                self.status_write[0][0] = -1
+            self.status_write[0][0] = -1  # Close connection
         self.logger.info('Thread : release write buffer end '+str(self.status_write[0][0]))
 
+    # Management of internal reading buffer
     def ready_to_read(self):
+        """
+        wait until it's ready to read in the buffer
+        :return: if the communication is ending
+        """
         self.logger.info('Thread : ready read buffer wait '+str(self.status_read[0][0]))
-        while self.status_read[0][0] == -2: # FAT END POINT
-            # self.logger.info('Thread : statut '+str(self.status_read[0][0]))
+        while self.status_read[0][0] == -2:  # wait the end of reading buffer
             time.sleep(0.001)
-            pass
         self.logger.info('Thread : ready read buffer end '+str(self.status_read[0][0]))
-        self.databuffer = self.buffer_read_data[0]
-        return self.status_read[0]
+        self.databuffer = self.buffer_read_data[0]  # write in the buffer
+        return self.status_read[0]  # return status
 
     def end_read(self):
+        """
+        end to read in the buffer
+        """
         self.logger.info('Thread : end read buffer '+str(self.status_read[0][0]))
         with self.lock_read:
             if self.status_read[0][0] != -1:
-                self.status_read[0][0] = -2
+                self.status_read[0][0] = -2  # end the reading of the buffer
         self.logger.info('Thread : end read buffer end '+str(self.status_read[0][0]))
 
     def release_read_buffer(self):
+        """
+        release reading buffer and send the end of the communication
+        """
         self.logger.info('Thread : release read buffer '+str(self.status_read[0][0]))
-        while self.status_read[0][0] == -2: # FAT END POINT
-            # self.logger.info('Thread : statut '+str(self.status_read[0][0]))
+        while self.status_read[0][0] == -2:  # wait until the written is ending
             time.sleep(0.001)
-            pass
         with self.lock_read:
-            self.status_read[0][0] = -1
+            self.status_read[0][0] = -1  # close the connection
         self.logger.info('Thread : release read buffer unlock '+str(self.status_read[0][0]))
 
-
+    # Section 1 : spike trains exchange
     def send_spikes_ready(self):
+        """
+        see super class
+        """
         self.logger.info(" Thread : ready send spikes")
         self.send_spike_exit = self.ready_write_buffer()
 
     def send_spikes(self):
+        """
+        see super class
+        """
         self.logger.info(" Thread : spike send")
-        self.end_writing()
+        self.end_write_buffer()
 
-    def send_spikes_trains(self,spike_trains):
+    def send_spikes_trains(self, spike_trains):
+        """
+        see super class
+        """
         self.logger.info('Thread : spike(send) : begin')
         self.send_spike_exit = self.ready_write_buffer()
         self.logger.info('Thread : spike(send) : buffer')
         if self.send_spike_exit:
             self.logger.info('Thread : spike(get) : receive end ')
             return
+        # create continue data with all spike trains
         data_shape = []
         for index, spike_train in enumerate(spike_trains):
             data_shape.append(len(spike_train))
             # self.logger.info("TRS : spike times : "+str(index)+" " +str(len(spike_train))+
             #                  "\n"+str(spike_train))
-        self.logger.info('Thread : spike(send) : '+str(np.concatenate(spike_trains)))
         data = np.concatenate(spike_trains)
-        self.logger.info('Thread : spike(send) : '+str(data.shape))
         self.shape_buffer = data_shape
-        if len(data) !=0:
+        # special case for empty data
+        if len(data) != 0:
             self.databuffer[:data.shape[0]] = data
         self.logger.info('Thread : spike(send) : data write')
-        self.end_writing()
+        self.end_write_buffer()
         self.logger.info('Thread : spike(send) : end')
 
-
     def send_spikes_end(self):
+        """
+        see super class
+        """
         self.logger.info("Thread : end send")
         self.release_write_buffer()
 
     def get_spikes(self):
+        """
+        see super class
+        """
         self.logger.info('Thread : spike(get) : begin ')
         # wait until the data are ready to use
         self.shape_buffer = self.ready_to_read()
@@ -132,31 +184,44 @@ class Thread_communication(CommunicationInternAbstract):
             self.logger.info('Thread : spike(get) : receive end ')
             return None
         self.logger.info('Thread : spike(get) : reshape data')
+        # reshape the data for use by the communicator with the simulator
         spikes_times = []
         index = 0
         for nb_spike in self.shape_buffer:
-            self.logger.info("nb spike shape :" +str(index))
+            self.logger.info("nb spike shape :" + str(index))
             spikes_times.append(self.databuffer[index:index + int(nb_spike)])
             index += int(nb_spike)
         self.logger.info('Thread : spike(get) : end reshape data')
         return spikes_times
 
     def get_spikes_ready(self):
+        """
+        see super class
+        """
         self.logger.info('Thread : spike(ready) : ready get spikes')
         self.shape_buffer = self.ready_to_read()
         self.logger.info('Thread : spike(ready) : ready to write : ' + str(self.shape_buffer))
 
     def get_spikes_release(self):
+        """
+        see super class
+        """
         self.logger.info('Thread : spike(release)')
         self.end_read()
         self.logger.info('Thread : spike(get) : end')
 
     def get_spikes_end(self):
+        """
+        see super class
+        """
         self.logger.info('Thread : spike(end) : begin')
         self.release_read_buffer()
         self.logger.info('Thread : spike(end) : end')
 
     def get_time_rate(self):
+        """
+        see super class
+        """
         self.logger.info("Thread : rate(get) : get time rate")
         self.shape_buffer = self.ready_to_read()
         if self.shape_buffer[0] == -1:
@@ -167,19 +232,28 @@ class Thread_communication(CommunicationInternAbstract):
         self.logger.info("Thread : rate(get) : data request : time :"+str(times))
         rate = self.buffer_read_data[0][1]
         self.logger.info("Thread : rate(get) : end")
-        return times,rate
+        return times, rate
 
     def get_time_rate_release(self):
+        """
+        see super class
+        """
         self.logger.info('Thread : rate(release)')
         self.end_read()
         self.logger.info('Thread : time(get) : end')
 
     def get_time_rate_end(self):
+        """
+        see super class
+        """
         self.logger.info('Thread : spike(end) : begin')
         self.release_read_buffer()
         self.logger.info('Thread : spike(end) : end')
 
     def send_time_rate(self, time_step, rate):
+        """
+        see super class
+        """
         self.logger.info('Thread : spike(send) : begin')
         self.send_time_rate_exit = self.ready_write_buffer()
         self.logger.info('Thread : spike(send) : buffer')
@@ -189,13 +263,16 @@ class Thread_communication(CommunicationInternAbstract):
         self.shape_buffer = [time_step.shape[0]]
         self.logger.info('Thread : spike(send) : time :'+str(time_step))
         self.logger.info('Thread : spike(send) : buffer :'+str(np.array(self.databuffer).shape))
-        self.databuffer = [time_step,rate]
+        self.databuffer = [time_step, rate]
         self.logger.info('Thread : spike(send) : data write')
-        self.end_writing()
+        self.end_write_buffer()
         self.logger.info('Thread : spike(send) : end')
 
     def send_time_rate_end(self):
-        self.logger.info("Thread : rate(end) : begin "+str( not self.send_time_rate_exit))
+        """
+        see super class
+        """
+        self.logger.info("Thread : rate(end) : begin "+str(not self.send_time_rate_exit))
         if not self.send_time_rate_exit:
             self.release_write_buffer()
         self.logger.info("Thread : rate(end) : end")
