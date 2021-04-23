0976c079759c0a07733ce7d7aecf6ffbcd415868
Kusch Lionel <lionel.kusch@univ-amu.fr>
2021-04-23 18:30:43 +0200

    remove dependency in launcher

--- nest_elephant_tvb/launcher/run_exploration.py
+++ nest_elephant_tvb/launcher/run_exploration.py
@@ -9,7 +9,40 @@ import subprocess
 import time
 import numpy as np
 import copy
-from nest_elephant_tvb.utils import create_logger
+import logging
+
+
+def create_logger(path, name, log_level):
+    """
+    creation of logger for module
+    :param path: path of the logger
+    :param name: name of the module
+    :param log_level: level of logger
+    :return:
+    """
+    # Configure logger
+    logger = logging.getLogger(name)
+    fh = logging.FileHandler(path+'/log/'+name+'.log')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    if log_level == 0:
+        fh.setLevel(logging.DEBUG)
+        logger.setLevel(logging.DEBUG)
+    elif  log_level == 1:
+        fh.setLevel(logging.INFO)
+        logger.setLevel(logging.INFO)
+    elif  log_level == 2:
+        fh.setLevel(logging.WARNING)
+        logger.setLevel(logging.WARNING)
+    elif  log_level == 3:
+        fh.setLevel(logging.ERROR)
+        logger.setLevel(logging.ERROR)
+    elif  log_level == 4:
+        fh.setLevel(logging.CRITICAL)
+        logger.setLevel(logging.CRITICAL)
+
+    return logger
 
 
 def run(parameters_file):
