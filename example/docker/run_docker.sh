258129692af23bbee05844979adc8557cfa19053
Kusch Lionel <lionel.kusch@univ-amu.fr>
2021-04-27 20:41:13 +0200

    Add example of docker

--- example/docker/run_docker.sh
+++ example/docker/run_docker.sh
@@ -4,12 +4,18 @@ CURRENT_REPERTORY=$(pwd)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR" || exit
 echo "doesn't work missing connection between docker"
-sudo --preserve-env=DIR,PATH -- sh -c "cd $DIR &&rm -rd */log */nest */translation */tvb"
-#
-sudo --preserve-env=DIR,PATH -- sh -c "cd $DIR && python3 ../../nest_elephant_tvb/launcher/run_exploration.py ./case_up_down/parameter.json"
-sudo --preserve-env=DIR,PATH -- sh -c "cd $DIR && python3 ../../nest_elephant_tvb/launcher/run_exploration.py ./case_asynchronous/parameter.json"
-sudo --preserve-env=DIR,PATH -- sh -c "cd $DIR && python3 ../../nest_elephant_tvb/launcher/run_exploration.py ./case_regular_burst/parameter.json"
-#
-#sudo --preserve-env=DIR,PATH -- sh -c "cd $DIR && rm -rd */log */nest */translation */tvb"
+set +x
+sudo echo "start"
+rm -rd ${DIR}/case_up_down/log ${DIR}/case_up_down/nest ${DIR}/case_up_down/translation ${DIR}/case_up_down/tvb
+sudo docker run --mount type=bind,source=${DIR}/case_up_down/,target=${DIR}/case_up_down/ local:NEST_TVB_IO_PAPER python3 /home/nest_elephant_tvb/launcher/run_exploration.py ${DIR}/case_up_down/parameter.json
+sudo chgrp -R ${USER} ${DIR}/case_up_down; sudo chown -R ${USER} ${DIR}/case_up_down/
+
+rm -rd ${DIR}/case_asynchronous/log ${DIR}/case_asynchronous/nest ${DIR}/case_asynchronous/translation ${DIR}/case_asynchronous/tvb
+sudo docker run --mount type=bind,source=${DIR}/case_asynchronous/,target=${DIR}/case_asynchronous/ local:NEST_TVB_IO_PAPER python3 /home/nest_elephant_tvb/launcher/run_exploration.py ${DIR}/case_asynchronous/parameter.json
+sudo chgrp -R ${USER} ${DIR}/case_asynchronous; sudo chown -R ${USER} ${DIR}/case_asynchronous/
+
+rm -rd ${DIR}/case_regular_burst/log ${DIR}/case_regular_burst/nest ${DIR}/case_regular_burst/translation ${DIR}/case_regular_burst/tvb
+sudo docker run --mount type=bind,source=${DIR}/case_regular_burst/,target=${DIR}/case_regular_burst/ local:NEST_TVB_IO_PAPER python3 /home/nest_elephant_tvb/launcher/run_exploration.py ${DIR}/case_regular_burst/parameter.json
+sudo chgrp -R ${USER} ${DIR}/case_regular_burst; sudo chown -R ${USER} ${DIR}/case_regular_burst
 
 cd $CURRENT_REPERTORY
--- example/local/run_local.sh
+++ example/local/run_local.sh
@@ -5,8 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR" || exit
 
 rm -rd ./case_up_down/log ./case_up_down/nest ./case_up_down/translation ./case_up_down/tvb
-rm -rd ./case_asynchronous/log ./case_asynchronous/nest ./case_asynchronous/translation */tvb
-rm -rd ./case_regular_burst/log ./case_regular_burst/nest ./case_regular_burst/translation */tvb
+rm -rd ./case_asynchronous/log ./case_asynchronous/nest ./case_asynchronous/translation ./case_asynchronous/tvb
+rm -rd ./case_regular_burst/log ./case_regular_burst/nest ./case_regular_burst/translation ./case_regular_burst/tvb
 
 . ./../../tests/init.sh
 
@@ -15,7 +15,7 @@ python3 ../../nest_elephant_tvb/launcher/run_exploration.py ./case_asynchronous/
 python3 ../../nest_elephant_tvb/launcher/run_exploration.py ./case_regular_burst/parameter.json
 
 #rm -rd ./case_up_down/log ./case_up_down/nest ./case_up_down/translation ./case_up_down/tvb
-#rm -rd ./case_asynchronous/log ./case_asynchronous/nest ./case_asynchronous/translation */tvb
-#rm -rd ./case_regular_burst/log ./case_regular_burst/nest ./case_regular_burst/translation */tvb
+#rm -rd ./case_asynchronous/log ./case_asynchronous/nest ./case_asynchronous/translation ./case_asynchronous/tvb
+#rm -rd ./case_regular_burst/log ./case_regular_burst/nest ./case_regular_burst/translation ./case_regular_burst/tvb
 
 cd $CURRENT_REPERTORY
