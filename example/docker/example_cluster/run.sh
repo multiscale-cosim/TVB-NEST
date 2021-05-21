fe60d0a60492b088463598660116b8c42ade4367
Kusch Lionel <lionel.kusch@univ-amu.fr>
2021-05-21 10:13:35 +0200

    Remove the docker

--- example/docker/example_cluster/run.sh
+++
@@ -1,6 +0,0 @@
-module load daint-gpu
-module load sarus
-module load cray-python
-rm -rd case_asynchronous/log case_asynchronous/nest case_asynchronous/tvb case_asynchronous/translation 
-salloc -p debug -n 19 -N 6 -C gpu -A ich001 python launcher.py case_asynchronous/parameter.json
-
