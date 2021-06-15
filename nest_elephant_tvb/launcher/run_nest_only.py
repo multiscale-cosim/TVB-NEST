d546fed98f2d6e494ba14b796a1114bd11652bfe
Kusch Lionel <lionel.kusch@univ-amu.fr>
2021-06-15 16:44:50 +0200

    Add new launcher

---
+++ nest_elephant_tvb/launcher/run_nest_only.py
@@ -0,0 +1,10 @@
+from nest_elephant_tvb.Nest.simulation_Zerlaut import run_normal
+import sys
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) == 2:
+        run_normal(sys.argv[1])
+    else:
+        raise Exception('not good number of argument ')
