4eaad85bfb5c15529be559835447aeb7c152dded
Kusch Lionel <lionel.kusch@univ-amu.fr>
2021-07-03 16:42:47 +0200

    Configure install

--- install/local/install_ubuntu.sh
+++ install/local/install_ubuntu.sh
@@ -21,12 +21,32 @@
 # Script needs to be started from the directory it is located in
 CURRENT_REPERTORY=$(pwd)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR" || exit
+cd $DIR || exit
 
-# copy the folder for compiling in the docker
-cp -r  ../../nest-io-dev .
-sudo docker build -t local:NEST_TVB_IO_2 -f Nest_TVB_2.dockerfile .
-rm -rd nest-io-dev
+PATH_LIB=$DIR/../../lib/
+mkdir $PATH_LIB
+cd $PATH_LIB
 
-# return to the calling repertory
-cd "${CURRENT_REPERTORY}" || exit
+wget http://www.mpich.org/static/downloads/3.4.2/mpich-3.4.2.tar.gz
+tar -xf mpich-3.4.2.tar.gz
+cd mpich-3.4.2
+./configure --prefix=$DIR/../../lib/soft/ --with-device=ch4:ofi
+make
+make install
+cd ..
+rm -rdf mpich-3.4.2 mpich-3.4.2.tar.gz
+
+wget https://www.python.org/ftp/python/3.8.10/Python-3.8.10.tar.xz
+tar -xf Python-3.8.10.tar.xz
+cd Python-3.8.10
+./configure --enable-optimizations --enable-shared --prefix=$DIR/../../lib/soft/
+make
+make install
+cd ..
+rm -rdf  Python-3.8.10.tar.xz Python-3.8.10
+
+export PATH=$DIR/../../lib/soft/bin:$PATH
+export LD_LIBRARY_PATH=$DIR/../../lib/soft/lib/:$LD_LIBRARY_PATH
+export PYTHONPATH=$DIR/../../lib/soft/lib/python3.8/:$PYTHONPATH
+
+sh $CURRENT_REPERTORY/../py_venv/create_virtual_python.sh
--- install/py_venv/create_virtual_python.sh
+++ install/py_venv/create_virtual_python.sh
@@ -17,20 +17,82 @@
 #KIND, either express or implied.  See the License for the
 #specific language governing permissions and limitations
 #under the License.
+set -x
 
-pip3 install virtualenv
-virtualenv -p python3 --system-site-packages tvb_nest_lib
-source tvb_nest_lib/bin/activate
+PATH_LIB=$(pwd)
+pip3.8 install virtualenv
+virtualenv -p python3.8 --system-site-packages lib_py
+source $PATH_LIB/lib_py/bin/activate
 
-pip install nose
-pip install numpy cython Pillow
-pip install matplotlib
-pip install mpi4py
-pip install scipy 
-pip install elephant
+pip install --upgrade pip==21.1.2
+pip install nose==1.3.7
+pip install numpy==1.20.3 cython==0.29.23 Pillow==8.2.0
+pip install mpi4py==3.0.3
+pip install scipy==1.6.3
+pip install elephant==0.10.0
+# for plotting function
+pip install matplotlib==3.4.2
+pip install networkx==2.5.1
+pip install h5py==3.2.1
+pip install cycler==0.10.0
+pip install jupyter==1.0.0
+pip install vtk==9.0.1
 
-pip install tvb-data tvb-gdist tvb-library
+# install TVB
+pip install tvb-data==2.0 tvb-gdist==2.1.0 tvb-library==2.0.10
 
-# need to add the library for nest
+# for LFPY
+## install parameters
+git clone https://github.com/NeuralEnsemble/parameters
+cd parameters
+git checkout b95bac2bd17f03ce600541e435e270a1e1c5a478
+python3.8 setup.py install
+cd ..
+rm -rfd parameters
+## install neurons
+git clone https://github.com/neuronsimulator/nrn.git
+cd nrn
+git checkout 8.0.0
+mkdir build
+cd build
+cmake .. \
+-DNRN_ENABLE_INTERVIEWS=OFF \
+-DNRN_ENABLE_MPI=OFF \
+-DNRN_ENABLE_RX2D=OFF \
+-DCMAKE_INSTALL_PREFIX=$PATH_LIB/soft
+cmake --build . --parallel 7 --target install
+cd ../..
+rm -rfd nrn
+export PYTHONPATH=$PATH_LIB/soft/lib/python/:$PYTHONPATH
+## install LFPy
+pip install lfpykit==0.3
+pip install MEAutility==1.4.9
+pip install LFPy==2.2.1
+# install HybridLFPy from github
+git clone --branch nest-3-lio https://github.com/lionelkusch/hybridLFPy.git
+cd hybridLFPy
+python3.8 setup.py install
+cd ..
+rm -rfd hybridLFPy
 
+
+
+mkdir nest
+mkdir nest_build
+cd "nest_build" || exit
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$PATH_LIB/nest $PATH_LIB/../nest-io-dev \
+      -Dwith-mpi=ON \
+      -Dwith-openmp=ON \
+      -Dwith-readline=On \
+      -Dwith-ltdl=ON \
+      -Dwith-python=ON \
+      -Dcythonize-pynest=ON
+make
+make install
+cd ..
+rm -rd nest_build
+
+echo "export PYTHONPATH=$PATH_LIB/soft/lib/python/:$PATH_LIB/nest/lib/python3.8/site-packages/:$PYTHONPATH" >> $PATH_LIB/lib_py/bin/activate
+echo "export PATH=$PATH_LIB/soft/bin/:$PATH_LIB/nest/bin:$PATH" >> $PATH_LIB/lib_py/bin/activate
+echo "export LD_LIBRARY_PATH=$PATH_LIB/soft/lib:$LD_LIBRARY_PATH" >> $PATH_LIB/lib_py/bin/activate
 deactivate
